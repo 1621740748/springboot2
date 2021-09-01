874a874544f4fcb8cc9ad13ae2c39022b15a82ea
huangyan <1621740748@qq.com>
2021-09-01 15:04:33 +0800

    提交

--- src/main/java/com/cn/controller/MyController.java
+++ src/main/java/com/cn/controller/MyController.java
@@ -1,11 +1,19 @@
 package com.cn.controller;
 
-import com.cn.bean.City;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Map;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.parser.Feature;
+import com.cn.bean.City;
 
 /**
  * Created by Administrator on 1/18/2017.
@@ -26,5 +34,12 @@ public class MyController {
         city.setName("上海");
         city.setCityCode("0212");
         return city;
+    }
+    @RequestMapping("/getCity1")
+    @ResponseBody
+    public String  getCity1(HttpServletRequest req, HttpServletResponse resp) {
+      String data = req.getParameter("data");
+       JSONObject obj = JSON.parseObject(data, Feature.SupportNonPublicField);
+       return "index";
     }
 }
